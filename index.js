de5c7e3709bf2f40207273d1ac11c78a89064268
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-29 15:04:01 +0100

    docs: add comments

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
+// index.js
+
+// Import dotenv and express dependencies
 import 'dotenv/config';
 import express from 'express';
 
+// Create an express app assigned to 'app' variable
 const app = express();
 
+// Define a route handler for the default home page
 app.get('/', (req, res) => {
   res.send('Hello World!');
   } 
 );
 
+// Define the port to listen on
 const port = process.env.PORT || 3000;
+
+// Start the server on port 3000
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
