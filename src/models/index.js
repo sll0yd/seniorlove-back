853dadbec06162e2f53952478435d3a797baa106
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-30 18:35:23 +0100

    feat: enable testimonies and sentences to be called from the same files than the other models

--- src/models/index.js
+++ src/models/index.js
@@ -1,7 +1,7 @@
 // index.js
 
 // import models with associations from association.js
-import {sequelize, Users, Event, Tag, Message} from './association.js';
+import {sequelize, Users, Event, Tag, Message, Testimony, Sentence} from './association.js';
 
 // export models to be used in controllers
-export {sequelize, Users, Event, Tag, Message};
+export {sequelize, Users, Event, Tag, Message, Testimony, Sentence};
