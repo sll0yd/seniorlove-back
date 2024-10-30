14738f600100e8c93177eb4f632627dd6aee735f
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-30 10:18:57 +0100

    feat: implement event model

---
+++ src/models/event.model.js
@@ -0,0 +1,45 @@
+// event.model.js
+
+// Import necessary dependencies
+import { Model, DataTypes } from 'sequelize';
+import { sequelize } from './sequelize-client.js';
+
+// Create a new Sequelize model
+export class Event extends Model {}
+
+// Initialize the model with the appropriate configuration (regarding create_tables.sql)
+Event.init (
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    picture: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    date: {
+      type: DataTypes.STRING,
+      allowNull: false  
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    creator_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  },
+    {
+    sequelize,
+    tableName: 'event'
+  }
+)
+
+
+
