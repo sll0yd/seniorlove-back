d1f938ab85f51ade34330eeb70f7fdde77d387fe
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-30 10:19:43 +0100

    feat: implement message model

---
+++ src/models/message.model.js
@@ -0,0 +1,30 @@
+// message.model.js
+
+// Import necessary dependencies
+import {Model, DataTypes} from 'sequelize';
+import {sequelize} from './sequelize-client.js';
+
+// Create a new Sequelize model
+class Message extends Model {}
+
+// Initialize the model with the appropriate configuration (regarding create_tables.sql)
+Message.init(
+  {
+    sender_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    receiver_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    }
+  },
+  {
+    sequelize,
+    tableName: 'message'
+  }
+)
