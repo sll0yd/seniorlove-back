c502c3cc65ff361d585dfc192bca64abd5778058
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-30 18:19:20 +0100

    feat: enable sequelize client to read created_at and updated_at

--- src/models/sequelize-client.js
+++ src/models/sequelize-client.js
@@ -2,7 +2,12 @@ import 'dotenv/config';
 import { Sequelize } from 'sequelize';
 
 // Create a new Sequelize instance
-export const sequelize = new Sequelize(process.env.PG_URL);
+export const sequelize = new Sequelize(process.env.PG_URL, {
+  define: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at'
+  }
+});
 
 // Test the connection (uncomment line 10 to 21 to test)
 // test commande : node src/models/sequelize-client.js
