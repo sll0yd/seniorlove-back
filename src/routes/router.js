8f5b05c9c305c79984f8ba15401444fe369db31d
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-10-31 19:16:42 +0100

    chore: add new routes

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
+// Import the controllerWrapper function
+// The controllerWrapper function is used to wrap the controller methods
+// It catches any errors that occur in the controller methods and sends a 500 status code with an error message
+// It makes it possible not to repeat the try-catch block in each controller method
+import { controllerWrapper as cw} from './controller-wrapper.js';
 import authController from '../controllers/authController.js';
+import tagController from '../controllers/tagController.js';
 
 const router = Router();
 
-router.post('/signup', authController.createUser);
-router.post('/login', authController.loginUser);
+router.post('/signup', cw(authController.createUser));
+router.post('/login', cw(authController.loginUser));
+
+router.get('/tags', cw(tagController.getTags));
 
 export default router;
