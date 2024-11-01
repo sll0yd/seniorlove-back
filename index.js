657de4a2f0d7e0ee84da63af850104d8372b6d1f
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 12:01:18 +0100

    chore: format code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // index.js
 
 // Import dotenv and express dependencies
-import 'dotenv/config';
-import express from 'express';
-import router from './src/routes/router.js';
+import "dotenv/config";
+import express from "express";
+import router from "./src/routes/router.js";
 
 // Create an express app assigned to 'app' variable
 const app = express();
@@ -11,16 +11,16 @@ const app = express();
 // Define a route handler for the default home page
 // app.get('/', (req, res) => {
 //   res.send('Hello World!');
-//   } 
+//   }
 // );
 
 app.use(express.json());
-app.use('/api', router);
+app.use("/api", router);
 
 // Define the port to listen on
 const port = process.env.PORT || 3000;
 
 // Start the server on port 3000
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+	console.log(`Server is running on port ${port}`);
 });
