26ae3f2fcd384e271de5d8820b8f5730ffc9e35d
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 11:57:20 +0100

    chore: format code

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -1,30 +1,30 @@
 // message.model.js
 
 // Import necessary dependencies
-import {Model, DataTypes} from 'sequelize';
-import {sequelize} from './sequelize-client.js';
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "./sequelize-client.js";
 
 // Create a new Sequelize model
 export class Message extends Model {}
 
 // Initialize the model with the appropriate configuration (regarding create_tables.sql)
 Message.init(
-  {
-    sender_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    receiver_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false
-    },
-    content: {
-      type: DataTypes.TEXT,
-      allowNull: false
-    }
-  },
-  {
-    sequelize,
-    tableName: 'message'
-  }
-)
+	{
+		sender_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		receiver_id: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+		content: {
+			type: DataTypes.TEXT,
+			allowNull: false,
+		},
+	},
+	{
+		sequelize,
+		tableName: "message",
+	},
+);
