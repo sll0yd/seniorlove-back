4affc52bfe5933879824855a1532d1bb52060231
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 16:36:04 +0100

    docs: add comments

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
+// Import and use all routes here
 import { Router } from "express";
 import authRoute from "./auth.router.js";
 import tagRoute from "./tag.router.js";
 import usersRoute from "./users.router.js";
 import eventRoute from "./event.router.js";
 
+// Create a new router
 export const router = Router();
 
+// Use all routes
 router.use(authRoute);
 router.use(tagRoute);
 router.use(usersRoute);
