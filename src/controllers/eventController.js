270ee84fc63e0e8d847128e773b7861fbea071f8
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 15:45:17 +0100

    chore: validate parsed provided id + return 404 status if not found

--- src/controllers/eventController.js
+++ src/controllers/eventController.js
@@ -3,19 +3,19 @@ import { Event, Tag, Users } from "../models/index.js";
 const eventController = {
 	async getAllEvents(req, res) {
 		const events = await Event.findAll({
-      // We want to retrieve the tags associated with the events
+			// We want to retrieve the tags associated with the events
 			include: [
 				{
-          // Specify the model to use
+					// Specify the model to use
 					model: Tag,
-          // Specify the alias to use (refer to the association in the models)
+					// Specify the alias to use (refer to the association in the models)
 					as: "tags",
-          // Specify the attributes to retrieve from the Tag model
-          attributes: ["id", "name"],
-          // Define the attributes to retrieve from the intermediate model (e.g. events_tags)
-          // Not doing this will return all attributes from the intermediate model by default and we want to exclude them
+					// Specify the attributes to retrieve from the Tag model
+					attributes: ["id", "name"],
+					// Define the attributes to retrieve from the intermediate model (e.g. events_tags)
+					// Not doing this will return all attributes from the intermediate model by default and we want to exclude them
 					through: {
-            // In this case, we don't need any attributes from the intermediate model, so we exclude them using an empty array
+						// In this case, we don't need any attributes from the intermediate model, so we exclude them using an empty array
 						attributes: [],
 					},
 				},
@@ -24,37 +24,51 @@ const eventController = {
 		res.json(events);
 	},
 
-  async getOneEvent(req, res) {
-    const event = await Event.findByPk(req.params.id, {
-      // We want to retrieve the tags associated with the event
-      include: [
-        {
-          // Specify the model to use
-          model: Tag,
-          // Specify the alias to use (refer to the association in the models)
-          as: "tags",
-          // Specify the attributes to retrieve from the Tag model
-          attributes: ["id", "name"],
-          // Define the attributes to retrieve from the intermediate model (e.g. events_tags)
-          // Not doing this will return all attributes from the intermediate model by default and we want to exclude them
-          through: {
-            // In this case, we don't need any attributes from the intermediate model, so we exclude them using an empty array
-            attributes: [],
-          },
-        },
-        {
-          model: Users,
-          as: "participants",
-          attributes: ["id", "userName", "picture"],
-          through: {
-            attributes: [],
-          },
-        },
-      ],
-    });
-    console.log(event.participants.length)
-    res.json(event);
-  },
+	async getOneEvent(req, res) {
+    // Get the event ID from the URL
+		const id = Number.parseInt(req.params.id);
+
+    // Check if the ID is a valid number
+		if (Number.isNaN(id)) {
+			return res.status(400).json({ error: "Invalid event ID" });
+		}
+
+		const event = await Event.findByPk(id, {
+			// We want to retrieve the tags associated with the event
+			include: [
+				{
+					// Specify the model to use
+					model: Tag,
+					// Specify the alias to use (refer to the association in the models)
+					as: "tags",
+					// Specify the attributes to retrieve from the Tag model
+					attributes: ["id", "name"],
+					// Define the attributes to retrieve from the intermediate model (e.g. events_tags)
+					// Not doing this will return all attributes from the intermediate model by default and we want to exclude them
+					through: {
+						// In this case, we don't need any attributes from the intermediate model, so we exclude them using an empty array
+						attributes: [],
+					},
+				},
+				{
+					model: Users,
+					as: "participants",
+					attributes: ["id", "userName", "picture"],
+					through: {
+						attributes: [],
+					},
+				},
+			],
+		});
+
+    // If the event does not exist, return a 404 status code with an error message
+		if (!event) {
+			return res.status(404).json({ error: "Event not found" });
+		}
+
+		console.log(event.participants.length);
+		res.json(event);
+	},
 };
 
 export default eventController;
