207b8cd01c9556a2abd4716e760e1779ecf8f532
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 16:50:04 +0100

    feat: add sentence router in routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import tagRoute from "./tag.router.js";
 import usersRoute from "./users.router.js";
 import eventRoute from "./event.router.js";
 import testimonyRoute from "./testimony.router.js";
+import sentenceRoute from "./sentence.router.js";
 
 // Create a new router
 export const router = Router();
@@ -15,3 +16,4 @@ router.use(tagRoute);
 router.use(usersRoute);
 router.use(eventRoute);
 router.use(testimonyRoute);
+router.use(sentenceRoute);
