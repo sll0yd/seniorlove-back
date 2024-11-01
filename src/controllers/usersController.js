1413919fe9251488006bdae3942bf80d86f98579
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-01 11:54:02 +0100

    feat: add method to get all users

---
+++ src/controllers/usersController.js
@@ -0,0 +1,15 @@
+import { Users, Tag } from "../models/index.js";
+
+const usersController = {
+	async getAllUsers(req, res) {
+		const users = await Users.findAll({
+			include: {
+				model: Tag,
+				as: "tags",
+			}
+		});
+		res.json(users);
+	},
+};
+
+export default usersController;
