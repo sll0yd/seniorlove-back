54e0f93be86ac6b5d9d7d0c8b76b0afbcc1a80b5
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-02 17:29:16 +0100

    chore: parse and validate the provided id through the token

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -3,7 +3,13 @@ import { Users, Tag} from "../models/index.js";
 const meController = {
 
 	async getSelfProfile(req, res) {
-		const user = await Users.findByPk(req.user.id, {
+		const id = Number.parseInt(req.user.id, 10);
+
+		if (Number.isNaN(id)) {
+			return res.status(400).json({ error: "Invalid user id" });
+		}
+		
+		const user = await Users.findByPk(id, {
 			include: {
 				model: Tag,
 				as: "tags",
