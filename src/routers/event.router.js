5abc688aa6c3a9f50a11ba962e0ced1ab2d21ba0
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-03 15:13:19 +0100

    feat: create endpoint to create an event

--- src/routers/event.router.js
+++ src/routers/event.router.js
@@ -2,6 +2,7 @@
 import { Router } from "express";
 import { controllerWrapper as cw } from "./controller-wrapper.js";
 import eventController from "../controllers/eventController.js";
+import isLoggedIn from "../middlewares/isLoggedIn.js";
 
 // Create a new router
 const router = Router();
@@ -9,6 +10,7 @@ const router = Router();
 // Use all routes
 router.get("/events", cw(eventController.getAllEvents));
 router.get("/events/:id", cw(eventController.getOneEvent));
+router.post("/events", isLoggedIn, cw(eventController.createAnEvent));
 
 // Export the router
 export default router;
