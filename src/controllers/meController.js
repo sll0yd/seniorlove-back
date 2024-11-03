1746824797bec4a9f92fb7b882769c392cce6dc6
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-03 22:53:50 +0100

    feat: allow user to add tag to its profile

--- src/controllers/meController.js
+++ src/controllers/meController.js
@@ -52,7 +52,6 @@ const meController = {
 
 		res.json(user);
 	},
-
 	async deleteSelfProfile(req, res) {
 		const id = Number.parseInt(req.user.id, 10);
 
@@ -64,7 +63,51 @@ const meController = {
 		
 		res.json({ message: "User deleted" });
 	},
+	async asignTagToSelfProfile(req, res) {
+		const id = Number.parseInt(req.user.id, 10);
+		const tagId = Number.parseInt(req.body.tagId, 10);
 
+		if (Number.isNaN(id)) {
+			return res.status(400).json({ error: "Invalid user id" });
+		}
+
+		if (Number.isNaN(tagId)) {
+			return res.status(400).json({ error: "Invalid tag id" });
+		}
+
+		const tag = await Tag.findByPk(tagId);
+
+		if (!tag) {
+			return res.status(404).json({ error: "Tag not found" });
+		}
+
+		const user = await Users.findByPk(id, {
+			include: {
+				model: Tag,
+				as: "tags",
+				attributes: ["id", "name"],
+				through: {
+					attributes: [],
+				},
+			},
+		});
+		
+		if (!user) {
+			return res.status(404).json({ error: "User not found" });
+		}
+
+		// Check if the tag is already assigned to the user
+		// If the tag is already assigned, return a 400 status code with an error message
+		// The some method checks if at least one element in the array satisfies the condition
+		if (user.tags.some((t) => t.id === tag.id)) {
+			return res.status(400).json({ error: "Tag already assigned" });
+		}
+
+		// Add the tag to the user using the addTag method
+		await user.addTag(tag);
+
+		res.json({ message: "Tag added" });
+	},	
 };
 
 export default meController;
