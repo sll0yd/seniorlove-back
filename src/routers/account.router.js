faf19a8d4ce27ee98ab9be248451bd42fc94d9b6
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-04 13:50:18 +0100

    feat: branch add a tag to self created event method to account router

--- src/routers/account.router.js
+++ src/routers/account.router.js
@@ -17,5 +17,6 @@ router.get("/me/events", isLoggedIn, cw(meController.getSelfCreatedEvents));
 router.get("/me/events/:eventId", isLoggedIn, cw(meController.getOneOwnedEvent));
 router.patch("/me/events/:eventId", isLoggedIn, cw(meController.updateOwnedEvent));
 router.delete("/me/events/:eventId", isLoggedIn, cw(meController.deleteOwnedEvent));
+router.post("/me/events/:eventId/tags/:tagId", isLoggedIn, cw(meController.addTagToOwnedEvent));
 
 export default router;
