e87721be8bb9dbd59b2cd2e2376cc916ce52592b
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-04 19:27:46 +0100

    feat: connect get a sentence method to sentence router

--- src/routers/sentence.router.js
+++ src/routers/sentence.router.js
@@ -5,5 +5,6 @@ import sentenceController from "../controllers/sentenceController.js";
 const router = Router();
 
 router.get("/sentences", cw(sentenceController.getAllSentences));
+router.get("/sentences/:id", cw(sentenceController.getOneSentence));
 
 export default router;
