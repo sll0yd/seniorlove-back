09c0e0e0d521c980b0461308be1c005e5a389f7a
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-15 18:20:19 +0100

    feat: import session + adminRouter | use session | define url encoded | define static public use to serve ejs views | declare view engine | use API administration endpoint and serve the connexion view

--- index.js
+++ index.js
@@ -4,31 +4,44 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import session from "express-session";
 import { router as apiRouter } from "./src/routers/index.js";
+import { router as adminRouter } from "./src/routers/admin.index.js";
 
-// Create an express app assigned to 'app' variable
-const app = express();
+const app = express(); // Create an express app assigned to 'app' variable
 
-// Define a route handler for the default home page
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-//   }
-// );
+app.set("view engine", "ejs"); // Set the view engine to EJS
 
-// Enable All CORS Requests
-app.use(cors("*"));
+app.set("views","views"); // Set the views directory to the views folder
 
-// Parse JSON request bodies
-app.use(express.json());
+app.use(cors("*")); // Enable CORS
 
-// Serve static files from the uploads directory (for profile pictures)
-app.use("/uploads", express.static("./uploads"));
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
+
+app.use(express.json()); // Parse JSON request bodies
+
+app.use(
+	session({
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { secure: false },
+	})
+);
+
+app.use(express.static("public")); // Serve static files from the public directory
+
+app.use("/uploads", express.static("./uploads")); // Serve static files from the uploads directory (for profile pictures)
+
+app.get("/", (req, res) => {
+	res.render("connexion");
+});
 
 // Mount the API router at the /api endpoint
+app.use("/admin", adminRouter);
 app.use("/api", apiRouter);
 
-// Define the port to listen on
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; // Define the port to listen on
 
 // Start the server on port 3000
 app.listen(port, () => {
