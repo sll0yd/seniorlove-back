64c9f0f8def86e80abc7d3195f8e689cd8d8a767
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-17 18:39:30 +0100

    feat: define requests rate limiter

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-// index.js
-
-// Import dotenv and express dependencies
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import session from "express-session";
 import { router as apiRouter } from "./src/routers/index.js";
 import { router as adminRouter } from "./src/routers/admin.index.js";
+import { rateLimit } from 'express-rate-limit'
 
 const app = express(); // Create an express app assigned to 'app' variable
 
@@ -17,6 +15,11 @@ app.set("view engine", "ejs"); // Set the view engine to EJS
 app.set("views","views"); // Set the views directory to the views folder
 
 app.use(cors("*")); // Enable CORS
+
+app.use(rateLimit({
+	windowMs: 1 * 60 * 1000, // 1 minutes
+	max: 200, // Limit each IP to 100 requests per `window` (here, per 10 minutes)
+}));
 
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded request bodies
 
