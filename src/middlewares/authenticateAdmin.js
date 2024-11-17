a4b351417d824c34c1c0fb320401948bfcc73bba
LouisAntoninLesieur <louisantonin.lesieur@icloud.com>
2024-11-17 23:05:55 +0100

    feat: add 401 - handle style for back-office

--- src/middlewares/authenticateAdmin.js
+++ src/middlewares/authenticateAdmin.js
@@ -2,7 +2,7 @@ const authenticateAdmin = (req, res, next) => {
   if (req.session && req.session.admin) {
     next();
   } else {
-    res.status(401).json({ error: "Access denied. Please log in." });
+    res.status(401).render("401");
   }
 };
 
